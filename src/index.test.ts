e1faa367df82bcfcb812eed91340eb89585227a5
Gabriel Falcones <gabofalc@gmail.com>
2022-05-02 23:50:38 -0500

    init express server

---
+++ src/index.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest'
+import {createServer} from './server'
+
+
+describe('GET /', function() {
+    it('responds with json', (done) => {
+        request(createServer())
+            .get('/')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err)
+                console.log(res.body)
+                expect(res.body).toMatchObject({'message': 'Home'})
+                done()
+            })
+    });
+});
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+
+const PORT = 3000
+
+export function createServer() {
+    const app = express()
+    app.use(express.json())
+
+    app.get('/', (_req, res) => {
+        console.log('someone pinged here!!')
+        res.send({message: 'Home'})
+    })
+    return app
+}
+
+
+export function start() {
+    const app = createServer()
+
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+}
+
