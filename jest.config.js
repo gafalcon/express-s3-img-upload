776001ea40160f8f05715e175146e679616c59f8
Gabriel Falcones <gabofalc@gmail.com>
2022-05-17 20:30:34 -0500

    get images API

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ["<rootDir>/.jest/setEnvVars.js"],
-  roots: ["<rootDir>/src/"]
+  roots: ["<rootDir>/src/"],
 };
--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -1,14 +1,38 @@
 import { DataSource } from "typeorm"
 
-const myDataSource = new DataSource({
-    type: "mongodb",
-    url: process.env.MONGODB_URL,
-    // host: process.env.MONGODB_HOST,
-    // port: parseInt(process.env.MONGODB_PORT || "27017"),
-    database: "s3-img-uploading",
-    entities: [__dirname + "/entity/*.{js,ts}"],
-    logging: true,
-    synchronize: true,
-})
+
+let myDataSource: DataSource
+
+if (process.env.ENVIRONMENT === "test") {
+    myDataSource = new DataSource({
+        type: "mongodb",
+        url: process.env.MONGODB_URL,
+        database: "s3-img-test",
+        entities: [__dirname + "/entity/*.{js,ts}"],
+        logging: true,
+        synchronize: true,
+    })
+}else{
+    myDataSource = new DataSource({
+        type: "mongodb",
+        url: process.env.MONGODB_URL,
+        database: "s3-img-uploading",
+        entities: [__dirname + "/entity/*.{js,ts}"],
+        logging: true,
+        synchronize: true,
+    })
+}
+
+export const initDB = async () => {
+    return myDataSource
+        .initialize()
+        .then(() => {
+            console.log("Data Source has been initialized!")
+        })
+        .catch((err) => {
+            console.error("Error during Data Source initialization:", err)
+        })
+}
+
 
 export default myDataSource
